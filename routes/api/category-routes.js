beba436dad8dcf75f3d391f1181037f1412f7f2d
Chase Miller <chasehmiller97@gmail.com>
2023-06-21 00:18:28 -0400

    finished models

---
+++ routes/api/category-routes.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  Category.findAll({
+    include:
+  })
+  // find all categories
+  // be sure to include its associated Products
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+});
+
+// create new
+router.post('/', (req, res) => {
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbCatData => res.json(dbCatData))
+  .catch( err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+// delete by id
+router.delete('/:id', (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCatData => {
+      if (!dbCatData){
+        res.status(404)._construct.json({message: 'No category found with that id presentated'});
+        return;
+      }
+      res.json(dbCatData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
+});
+
+module.exports = router;
